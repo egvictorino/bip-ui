68c8fb69b4134ea0a910a7a4500221cb3fe1c443
Eduardo González Victorino <eduardogonzalezvictorino@MacBook-Air-de-Eduardo.local>
2026-02-26 10:06:14 -0600

    feat: se agrega checkbox

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -4,3 +4,6 @@ export type { ButtonProps } from './components/Button';
 
 export { Input } from './components/Input';
 export type { InputProps, InputType } from './components/Input';
+
+export { Checkbox } from './components/Checkbox';
+export type { CheckboxProps } from './components/Checkbox';
